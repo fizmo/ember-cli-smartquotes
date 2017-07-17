362f77eb4b09ca4c0a6dc3529f13674851af2c76
Christopher Currie <christopher@currie.com>
2017-07-16 21:43:45 -0700

    Initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 /* eslint-env node */
 'use strict';
 
+const smartquotes = require('smartquotes');
+
+class SmartquotesPlugin {
+  constructor() {
+    this.syntax = null; // set by HTMLBars
+  }
+
+  transform(ast) {
+    const walker = new this.syntax.Walker();
+    walker.visit(ast, (node) => {
+      if (node.type === 'TextNode') {
+      node.chars = smartquotes(node.chars);
+    }
+  });
+    return ast;
+  }
+}
+
 module.exports = {
-  name: 'ember-cli-smartquotes'
+  name: 'ember-cli-smartquotes',
+
+  setupPreprocessorRegistry(type, registry) {
+    registry.add('htmlbars-ast-plugin', {
+      name: 'smartquotes',
+      plugin: SmartquotesPlugin,
+      baseDir: function() {
+        return __dirname;
+      }
+    })
+  },
 };
